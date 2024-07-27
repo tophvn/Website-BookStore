5cab3bc505402bd8805d4d10549307752c7d6627
HoangHai <hoanghai07077@gmail.com>
2024-07-27 17:29:44 +0700

    update config

--- config/config.php
+++ config/config.php
@@ -1,21 +1,11 @@
 <?php
-// Cấu hình kết nối cơ sở dữ liệu
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'shop_db';
-
-// Tạo kết nối
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = mysqli_connect('sql110.infinityfree.com', 'if0_36950872', '7BCkbaU02cm05I', 'if0_36950872_shop');
 
 // Kiểm tra kết nối
-if ($conn->connect_error) {
-    die('Kết nối không thành công: ' . $conn->connect_error);
+if (!$conn) {
+    die('Kết nối không thành công: ' . mysqli_connect_error());
 }
 
-// Thiết lập bộ ký tự
 $conn->set_charset("utf8mb4");
-
-// $base_url = 'index.php';
 //echo "Kết nối thành công";
 ?>
